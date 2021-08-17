9a49b95b764a6cf0617ef08c73cd964a7b9fe251
lipka-coder <aivenor88@gmail.com>
2021-08-17 22:55:35 +0900

    Added Search Engine function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import logo from "./logo.png";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            Coded by Kasia Podlipska
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import './Dictionary.css';
+
+export default function Dictionary (){
+    let [keyword, setKeyword] = useState(null);
+
+    function searchWord (event){
+        event.preventDefault();
+        alert(`Searching for ${keyword} definition.`)
+    }
+
+    function handleKyewordChange(event){
+        setKeyword(event.target.value)
+    }
+
+    return (
+        <div className="dictionary">
+            <form onSubmit={searchWord}>
+                <input type="search" 
+                placeholder="Type a word..." 
+                autoFocus={true}
+                onChange={handleKyewordChange}/>
+            </form>
+        </div>
+    )
+}
