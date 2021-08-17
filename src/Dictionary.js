8f3fc45c5f90bda3d38fdea2e7327b9f03eb0a55
lipka-coder <aivenor88@gmail.com>
2021-08-18 00:50:11 +0900

    Added dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,9 +5,15 @@ import './Dictionary.css';
 export default function Dictionary (){
     let [keyword, setKeyword] = useState(null);
 
+    function handleResponse (response){
+        console.log(response.data)
+    }
+    
     function searchWord (event){
         event.preventDefault();
-        alert(`Searching for ${keyword} definition.`)
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(handleResponse)
     }
 
     function handleKyewordChange(event){
