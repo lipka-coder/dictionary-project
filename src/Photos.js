1be9693f938786b9fe86f4c08c6124ee2f35e538
lipka-coder <aivenor88@gmail.com>
2021-08-29 23:42:34 +0900

    Resolved the bug in Photos

--- src/Photos.js
+++ src/Photos.js
@@ -9,7 +9,7 @@ export default function Photos (props){
 						{props.photos.map (function (photo, index){
 							return (
 							<div className="col-4" key={index}>
-								<img src={photo.src.landscape} className="img-fluid" />
+								<img src={photo.src.landscape} className="img-fluid" alt={photo.photographer} />
 							</div>
 							);
 						})}
