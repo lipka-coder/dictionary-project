2bd6e5d39b5a08185403adba769850f32bb9b3dc
lipka-coder <aivenor88@gmail.com>
2021-08-29 23:30:14 +0900

    Added Photos API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,21 +2,32 @@ import React, { useState } from "react";
 import axios from "axios";
 import './Dictionary.css';
 import Results from "./Results.js"
+import Photos from "./Photos.js";
 
 export default function Dictionary (){
     let [keyword, setKeyword] = useState(null);
 		let [definitions, setDefinitions] = useState(null);
+		let [photos, setPhotos] = useState(null);
 
-    function handleResponse (response){
+    function handleDictionaryResponse (response){
         setDefinitions(response.data[0])
     }
+
+		function handlePexelResponse (response) {
+			setPhotos(response.data.photos);
+		}
     
     function searchWord (event){
         event.preventDefault();
 
         // documentation: https://dictionaryapi.dev/e
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-        axios.get(apiUrl).then(handleResponse)
+        axios.get(apiUrl).then(handleDictionaryResponse)
+
+				let pexelApiKey = "563492ad6f917000010000016225669aaae7482da574eb6b1d2d2c28";
+				let pexelApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+				let headers = {Authorization: `Bearer ${pexelApiKey}`};
+				axios.get(pexelApiUrl, { headers: headers }).then(handlePexelResponse);
     }
 
     function handleKyewordChange(event){
@@ -40,6 +51,7 @@ export default function Dictionary (){
             </form>
 					</section>
           <Results result={definitions} /> 
+					<Photos photos={photos} />
         </div>
     )
 }
